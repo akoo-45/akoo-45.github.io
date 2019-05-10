8b6833b4de18d903848aeeafd5949ee0a43c545c
ashley.koo@columbia.edu <akoo-45>
2019-05-10 23:36:26 +0000

    colab -> github

--- hw4_text_classification/index.js
+++ hw4_text_classification/index.js
@@ -12,7 +12,7 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.' 
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.', 
   'example4':
       'At his first violent sin he had felt a wave of vitality pass out of him and had feared to find his body or his soul maimed by the excess.' 
 };
